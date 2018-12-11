18575496273abf8fef31ab85927ed2a54bb1dee7
vahidzee <vahid98zee@gmail.com>
2018-12-11 21:19:40 +0330

    comments begin

---
+++ src/Components/comments/CommentsList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <comments-card v-for="i in 10" :key="i" :commentUserName="'person' + i" commentsDate="2h" :commentsDescription="lorem"></comments-card>
+    </div>
+</template>
+
+<script>
+    import CommentsCard from "./CommentsCard";
+    export default {
+        name: "CommentsList",
+        components: {CommentsCard},
+        data() {
+            return {
+                lorem: 'one thing that it doesn’t handle that can drive developers (especially those experienced with SEO) mad. That is the process of updating the page title and metadata on route change. Now it makes sense that vue-router doesn’t mess with this. Vue tries to stay out of the head and body elements as much as possible, as they can be rather volatile. But often times you’ll want the title of the browser to change when the pixel'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/Pages/PageNotFound.vue
+++ src/Pages/PageNotFound.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        404
+        <comments-list></comments-list>
     </div>
 </template>
 
 <script>
+    import CommentsList from "../Components/comments/CommentsList";
     export default {
-        name: "PageNotFound"
+        name: "PageNotFound",
+        components: {CommentsList}
     }
 </script>
 
