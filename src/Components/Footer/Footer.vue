7efaafa8d19c2b0f2119d0465346319dc2d5cf81
vahidzee <vahid98zee@gmail.com>
2018-12-13 15:56:28 +0330

    NavBar & Footer Added

---
+++ src/Components/Footer/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+    div{
+        background-color: black;
+        width: 100vw;
+        height:100vh;
+
+    }
+</style>
