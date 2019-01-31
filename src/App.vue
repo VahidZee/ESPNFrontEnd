26e28b73a21e499574441bf3c4ec5cbb2e663e71
vahidzee <vahid98zee@gmail.com>
2019-02-01 03:14:39 +0330

    App:router:scroll to top & router-view transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <nav-bar background-image="url('./Images/bg36.jpeg')"></nav-bar>
-    <router-view></router-view>
+    <transition name="component-fade" mode="out-in">
+      <router-view
+      ></router-view>
+    </transition>
     <Footer background-image="url('./Images/bg36.jpeg')" ></Footer>
   </div>
 </template>
@@ -19,6 +22,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .component-fade-enter-active, .component-fade-leave-active {
+    transition: opacity 0.5s ease-in-out;
+  }
+  .component-fade-enter, .component-fade-leave-to
+    /* .component-fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
 </style>
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -4,5 +4,8 @@ import {routes} from './Routes';
 
 export const router = new VueRouter({
     mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+        return { x: 0, y: 0 }
+    },
     routes,
 });
