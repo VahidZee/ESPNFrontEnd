d2b2016d8c233954f859c6e48a05ee789bc8390e
vahidzee <vahid98zee@gmail.com>
2019-02-01 13:27:04 +0330

    comment list

--- src/Components/comments/CommentsList.vue
+++ src/Components/comments/CommentsList.vue
@@ -9,7 +9,18 @@
                                :comment-data="com"
                 ></comments-card>
             </sui-comment-group>
-                <sui-input placeholder="Add Comment" icon="send" style="width: 100%" />
+
+            <sui-form
+                    @submit.prevent="submitComment"
+                    v-if="this.$store.state.logged_in"
+            >
+                <sui-input
+                        placeholder="Add Comment"
+                        icon="send"
+                        style="width: 100%"
+                />
+            </sui-form>
+
 
         </sui-container>
     </div>
@@ -17,65 +28,68 @@
 
 <script>
     import CommentsCard from "./CommentsCard";
+
     export default {
         name: "CommentsList",
         components: {CommentsCard},
-        props : {
+        props: {
             width: {
-                type : String,
-                default : '100vw'
+                type: String,
+                default: '100vw'
             },
-            'background-image' : {
+            'background-image': {
                 type: String,
             },
-            height : {
-                type : String,
+            height: {
+                type: String,
                 default: '30vh'
             },
+            type: {
+                type: String,
+                default: 'N',
+            }
         },
         data() {
             return {
-                comments : []
+                comments: []
             }
         },
         methods: {
             generateData(i = 1, level = 1) {
                 let lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam consectetur explicabo facere facilis in inventore nihil quod temporibus velit? A accusamus ad consequuntur deserunt dolores harum reprehenderit! Eos, nostrum.\n';
                 let randNumber = Math.floor((Math.random() * 313) + 1) % 3;
-                if( level === 4 )
+                if (level === 4)
                     return null;
                 let obj = {
                     id: i,
-                    userName : 'User-' + Math.floor((Math.random() * 313) + 1) % 45,
-                    userPicture : '../Images/bg'+ Math.floor((Math.random() * 313) + 1) % 45 + '.jpeg',
-                    publishDate : new Date(),
-                    text: ( i % 3 === 1 ) ? lorem + lorem : ( i % 3 ) ? lorem + lorem + lorem : lorem,
-                    likesCount : Math.floor((Math.random() * 313) + 1),
-                    liked : false,
-                    replies : []
+                    userName: 'User-' + Math.floor((Math.random() * 313) + 1) % 45,
+                    userPicture: '../Images/bg' + Math.floor((Math.random() * 313) + 1) % 45 + '.jpeg',
+                    publishDate: new Date(),
+                    text: (i % 3 === 1) ? lorem + lorem : (i % 3) ? lorem + lorem + lorem : lorem,
+                    likesCount: Math.floor((Math.random() * 313) + 1),
+                    liked: false,
+                    replies: []
                 };
-                for( let x = 1 ; x <= randNumber ; x++ ) {
-                    let temp= this.generateData(i , level + 1)
-                    if(temp )
-                        obj.replies.push( temp );
+                for (let x = 1; x <= randNumber; x++) {
+                    let temp = this.generateData(i, level + 1)
+                    if (temp)
+                        obj.replies.push(temp);
 
                 }
                 return obj;
             },
             //Fetching Data
             fetchData() {
-                let randNumber = Math.floor((Math.random() * 313) + 1) % 10 + 1;
-                for( let x = 1 ; x <= randNumber ; x++ ){
-                    let temp= this.generateData(x , 1)
-                    if(temp )
-                        this.comments.push( temp );
-                }
+
             },
+            submitComment() {
+                //
+            }
         },
         computed: {
             commentListSizing() {
                 return {
-                    maxHeight : this.height
+                    maxHeight: this.height
                 }
             }
         },
@@ -89,8 +103,9 @@
     .comment-list {
         overflow-y: scroll;
     }
+
     ::-webkit-scrollbar {
-        width: 0px;  /* remove scrollbar space */
-        background: transparent;  /* optional: just make scrollbar invisible */
+        width: 0px; /* remove scrollbar space */
+        background: transparent; /* optional: just make scrollbar invisible */
     }
 </style>
